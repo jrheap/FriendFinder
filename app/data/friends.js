b55125e812bbc4346825c88c5c501376af544286
Jason <jasonrheap@gmial.com>
2018-07-21 23:37:13 -0600

    As far as i can go

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,15 +3,15 @@
 // how to use this..... 
 // friends[0]=bigfootobject
 // friends[1 = yeti object]
-var friends = [{
+var friendsData = [{
     name: 'Big Foot',
-    picture: 'http://www.laentradasecreta.com/wp-content/uploads/2014/07/bigfoot.jpg',
+    photo: 'http://www.laentradasecreta.com/wp-content/uploads/2014/07/bigfoot.jpg',
     score: ['5', '3', '2', '3', '4', '4', '3', '5', '5','1']
 },
 {
     name: 'Yeti',
-    picture: 'http://harrypotter.wikia.com/wiki/Yeti?file=Yeti_-_FB-C.png',
+    photo: 'http://harrypotter.wikia.com/wiki/Yeti?file=Yeti_-_FB-C.png',
     score: ['1', '4', '5', '3', '5', '2', '3', '2', '1','5']  
 }]
 // THis allows the friends varible to be used in other file pages
-module.exports = friends;
+module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 // Inside this file i need to create a function maybe two that will compare the scores of the friends object and
 // let the user know who he/she is most capibilty 
-var friends = ("../data/friends.js")
+var friendData = ("../data/friends.js")
 
 var express = require("express");
 
@@ -8,22 +8,26 @@ var app = express();
 
 module.exports = function (app) {
     // GET gets data from somewhere else
-    app.get("/api/friends", function (req, res) {
-
-        res.json(friends);
-        // console.log(friends);
-        
-    })
-};
-// post takes the user input and creates data
-app.post("api/friends", function(res,res){
-
-var bestFriend = [{
-    name: "",
-    photo: "",
-    friendDiffernce: 100,
     
+    app.post("api/friends", function(req,res){
+        // post takes the user input and creates data
+        console.log(friendData)
+        res.json(friendData);
+        console.log('couldnt think of anything')
+        
+        // This stores the user input into the data
+        
+        
+        var userData = req.body
+        var userScore = userData.score;
+        
+        var totalDifferance 
+    });
+// create for a foor loop that will run throuhg all the 
+// for(var i = 0; i < friendData.lenght; i++){
+//     var currentFriend = friendData[i]
+//     totalDifferance = 0
 
-}]
-
-})
+//     console.log(currentFriend)
+// }};
+};
